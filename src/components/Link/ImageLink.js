3c22928cbf75cf3cf881a71f34be6bc2a8eb0e32
Chanoch <chanoch@clearbrook.it>
2018-05-31 22:38:21 +0100

    Removed non-ready components.

--- src/components/Link/ImageLink.js
+++ src/components/Link/ImageLink.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+/**
+ * Renders an image with a surrounding hyperlink
+ * 
+ * Styles used are:
+ * 
+ * a {
+ * }
+ * 
+ * img {
+ * }
+ * 
+ * This will output the following block:
+ * 
+ *  <a href={link} title={linkTitle}>
+        <img src={src} alt={altText} />
+    </a>
+ */
 export default class ImageLink extends React.Component {
     constructor(props) {
         super(props);
--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
-import CallToAction from './components/CallToAction/CallToAction';
 import Declaration from './components/Declaration/Declaration';
 import Divider from './components/Divider/Divider';
-import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import ImageLink from './components/ImageLink/ImageLink';
+import ImageLink from './components/Link/ImageLink';
 import Layout from './components/Layout/Layout';
 import Navigation from './components/Navigation/Navigation';
-import PhoneLink from './components/PhoneLink/PhoneLink';
-import Quote from './components/Quote/Quote';
-import Signature from './components/Signature/Signature';
-import Testimonials from './components/Testimonials/Testimonials';
+import PhoneLink from './components/Link/PhoneLink';
 
-var Ccc = {
-    CallToAction,
+var Csc = {
     Declaration,
     Divider,
-    Footer,
     Header,
     ImageLink,
     Layout,
     Navigation,
     PhoneLink,
-    Quote,
-    Signature
 }
 
-module.exports = Ccc;
+module.exports = Csc;
